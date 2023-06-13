7df77315806ee608934db57ee77b9efd85cff034
dd114 <40179423+dd114@users.noreply.github.com>
2023-06-13 08:12:04 +0500

    Processing by RLS filter in Synthetic-signals.ipynb has been added

--- processing_signals.py
+++ processing_signals.py
@@ -46,6 +46,35 @@ def NLMS_1d(first_input_1d, second_input_1d, number_of_weights_1d, mu_0, epsilon
 
     return h_k
 
+def LMS_1d(first_input_1d, second_input_1d, number_of_weights_1d, mu_0, epsilon):
+
+    h_k = np.zeros(number_of_weights_1d).reshape(number_of_weights_1d, 1)
+
+    first_input_k = np.zeros(number_of_weights_1d)
+
+    for k in range(1, len(second_input_1d)):
+        first_input_k = np.roll(first_input_k, -1)
+        first_input_k[-1] = first_input_1d[k]
+
+        first_input_k_vec = first_input_k.reshape(number_of_weights_1d, 1)
+        first_input_k_vec_transp = first_input_k_vec.transpose()
+
+        P_k = second_input_1d[k] * first_input_k_vec
+
+        R_k = first_input_k_vec.dot(first_input_k_vec_transp)
+
+        # mu_k = mu_0 / ((first_input_k_vec_transp ** 2).sum() + epsilon)
+        mu_k = mu_0 / (3 * np.trace(R_k) + epsilon)
+        # mu_k = mu_0 / (3 * np.trace(R_k))
+
+        grad_J = -2 * (P_k - R_k.dot(h_k))
+        # print(mu_0 / (3 * np.trace(r)), mu_0 / (3 * np.trace(r) + epsilon), mu_0 / ((first_input_k ** 2).sum() + epsilon), grad_J)
+        # print(mu_k, np.trace(r), grad_J)
+
+        h_k -= mu_k * grad_J
+
+    return h_k
+
 def RLS(first_input_1d, second_input_1d, number_of_weights_1d, lambd=0.9, delta=1):
 
     h_k = np.zeros(number_of_weights_1d).reshape(number_of_weights_1d, 1)
@@ -91,6 +120,28 @@ def fitting_of_NLMS(first_input, second_input, number_of_weights, mu_0=0.9, epsi
             # weights2 = NLMS_1d(first_input[i, :], second_input[i, :], number_of_weights, mu_0,
             #                               epsilon).reshape(number_of_weights, 1)
             weights[:, i] = NLMS_1d(first_input[i, :], second_input[i, :], number_of_weights, mu_0, epsilon).flatten()
+
+            # weights = np.concatenate((weights, weights2), axis=1)
+
+
+    return weights
+
+def fitting_of_LMS(first_input, second_input, number_of_weights, mu_0=0.9, epsilon=1):
+    assert len(first_input.shape) == len(
+        second_input.shape), f'The dimensions of the parameters do not match {len(first_input.shape)} != {len(second_input.shape)}'
+
+    weights = np.empty((number_of_weights, first_input.shape[0]))
+
+    if len(first_input.shape) == 1: # signal depends on time
+        weights = LMS_1d(first_input, second_input, number_of_weights, mu_0, epsilon).flatten()
+    elif len(first_input.shape) == 2:  # signal depends on time and x
+        # weights = NLMS_1d(first_input[0, :], second_input[0, :], number_of_weights, mu_0,
+        #                                  epsilon).reshape(number_of_weights, 1)
+
+        for i in range(0, first_input.shape[0]):
+            # weights2 = NLMS_1d(first_input[i, :], second_input[i, :], number_of_weights, mu_0,
+            #                               epsilon).reshape(number_of_weights, 1)
+            weights[:, i] = LMS_1d(first_input[i, :], second_input[i, :], number_of_weights, mu_0, epsilon).flatten()
 
             # weights = np.concatenate((weights, weights2), axis=1)
 
